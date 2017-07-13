f7aa37e786919eb9c48f6a96976d1a1f261f360f
davis999 <davis.dai@go6d.com>
2017-07-13 10:22:19 +0800

    add cros config

--- src/main/java/com/umasuo/report/application/rest/DeviceReportController.java
+++ src/main/java/com/umasuo/report/application/rest/DeviceReportController.java
@@ -5,6 +5,7 @@ import com.umasuo.report.application.service.DeviceReportApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import static com.umasuo.report.infrastructure.Router.DEVICE_REPORT_ROOT;
 /**
  * Created by Davis on 17/6/15.
  */
+@CrossOrigin
 @RestController
 public class DeviceReportController {
 
